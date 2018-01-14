2cc51ee0260046bba5e024b6c5583e09558bc666
Eric0605 <tsang0605@gmail.com>
2018-01-13 17:58:51 -0800

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -55,7 +55,68 @@ var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 var HTMLonlinePic = '<img src="%data%">';
-
+var locations = [
+  {
+        title: 'Mira Loma High School',
+        location: {
+            lat: 38.6333032,
+            lng: -121.3704513
+        },
+        address: "4000 Edison Ave, Sacramento, CA 95821",
+        phone: "(916)971-7465",
+        url : ""
+    },
+    {
+        title: 'Dutch Brothers',
+        location: {
+            lat: 38.665217,
+            lng:  -121.342152
+        },
+        address: "5289 Auburn Blvd, Sacramento, CA 95841",
+        phone: "(541)955-4700",
+        url : ""
+    },
+    {
+        title: 'Thai Hut',
+        location: {
+            lat: 38.6597717,
+            lng: -121.3285258
+        },
+        address: "5800 Madison Ave k, Sacramento, CA 95841",
+        phone: "(916)349-8844",
+        url : ""
+    },
+    {
+        title: 'Arcade Creek Park',
+        location: {
+            lat: 38.66,
+            lng: -121.33
+        },
+        address: "5613 Omni Dr, Sacramento, CA 95841",
+        phone: "(916)482-8377",
+        url : ""
+    },
+    {
+        title: 'Christ Community Church',
+        location: {
+            lat: 38.657923,
+            lng: -121.328110
+        },
+        address: "United States, 5025 Manzanita Ave, Carmichael, CA 95608",
+        phone: "(916)344-2382",
+        url : ""
+    },
+    {
+        title: 'VCA Sacramento Animal Medical Group',
+        location: {
+            lat: 38.655541,
+            lng: -121.3271
+        },
+        address: "4990 Manzanita Ave, Carmichael, CA 95608",
+        phone: "(916)331-7430",
+        url : ""
+    },
+];
 var map;
 var markers = [];
 var placeMarkers = [];
