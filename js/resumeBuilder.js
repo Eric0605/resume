f882d381404b170fa2ef1a093b7c5f51419dd352
Eric0605 <tsang0605@gmail.com>
2018-01-13 22:25:34 -0800

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -96,7 +96,8 @@ var education = {
         for (var x = 0; x < education.schools.length; x++) {
             $(".education-entry:last").css("color", "blue");
             $("#education").append(HTMLschoolStart);
-            var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[x].name);
+            var formattedschoolLink = HTMLschoolName.replace("%link%", education.schools[x].url);
+            var formattedschoolName = formattedschoolLink.replace("%data%", education.schools[x].name);
             $(".education-entry:last").append(formattedschoolName);
             var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[x].degree);
             $(".education-entry:last").append(formattedschoolDegree);
