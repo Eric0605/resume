d4f857a1f7d87da4f8d1024d6dc9c40032b42420
Eric0605 <tsang0605@gmail.com>
2018-01-13 22:27:32 -0800

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -43,8 +43,7 @@ var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="%link%">%data%';
-var HTMLschoolDegree = '%data%</a>';
+var HTMLschoolName = '<a href="%link%">%data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 
