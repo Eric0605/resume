5668ae4238472cbfeec3dfedee21088b33ae7676
Eric0605 <tsang0605@gmail.com>
2018-01-13 21:56:43 -0800

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -43,14 +43,14 @@ var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#"><div>%data%<div></a>';
-var HTMLschoolDegree = '<div>%data%</div>';
+var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#"><div>%data%<div></a>';
-var HTMLonlineSchool = '<div>%data%</div>';
+var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 var HTMLonlinePic = '<img src="%data%">';
