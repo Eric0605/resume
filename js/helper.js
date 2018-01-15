c60d71a91baa392817c454ed76d75141b7e55827
Eric0605 <tsang0605@gmail.com>
2018-01-14 19:17:50 -0800

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -18,9 +18,9 @@ var HTMLheaderRole = '<span id="role">%data%</span><hr>';
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><a href="#"><span class="white-text">%data%</span></a></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><a href="#"><span class="white-text">%data%</span></a></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">Twitter</span><a href="%link%"><img class="logo" src="https://designshack.net/wp-content/uploads/larrybird-2.jpg"></img></a></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">GitHub</span><a href="%link%"><img class="logo" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"></img></a></li>';
-var HTMLlinkin = '<li class="flex-item"><span class="orange-text">LinkedIn</span><a href="%link%"><img class="logo" src="https://seeklogo.com/images/L/linkedin-icon-logo-FBADE03110-seeklogo.com.png"></img></a></li>';
+var HTMLtwitter = '<li class="flex-logo"><span class="orange-text">Twitter</span><a href="%link%"><img class="logo" src="https://designshack.net/wp-content/uploads/larrybird-2.jpg"></img></a></li>';
+var HTMLgithub = '<li class="flex-logo"><span class="orange-text">GitHub</span><a href="%link%"><img class="logo" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"></img></a></li>';
+var HTMLlinkin = '<li class="flex-logo"><span class="orange-text">LinkedIn</span><a href="%link%"><img class="logo" src="https://seeklogo.com/images/L/linkedin-icon-logo-FBADE03110-seeklogo.com.png"></img></a></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><a href="#"><span class="white-text">%data%</span></a></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
