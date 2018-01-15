06baf02a43d63ac68f050e2fa840caeb4fa4aacd
Eric0605 <tsang0605@gmail.com>
2018-01-14 19:25:40 -0800

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -13,14 +13,14 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span id="role">%data%</span><hr>';
+var HTMLheaderRole = '<span id="role">%data%</span>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><a href="#"><span class="white-text">%data%</span></a></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><a href="#"><span class="white-text">%data%</span></a></li>';
 var HTMLtwitter = '<li class="flex-logo"><span class="orange-text">Twitter</span><a href="%link%"><img class="logo" src="https://designshack.net/wp-content/uploads/larrybird-2.jpg"></img></a></li>';
 var HTMLgithub = '<li class="flex-logo"><span class="orange-text">GitHub</span><a href="%link%"><img class="logo" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"></img></a></li>';
-var HTMLlinkin = '<li class="flex-logo"><span class="orange-text">LinkedIn</span><a href="%link%"><img class="logo" src="https://seeklogo.com/images/L/linkedin-icon-logo-FBADE03110-seeklogo.com.png"></img></a></li>';
+var HTMLlinkin = '<li class="flex-logo"><span class="orange-text">LinkedIn</span><a href="%link%"><img class="logo" src="https://seeklogo.com/images/L/linkedin-icon-logo-FBADE03110-seeklogo.com.png"></img></a></li><hr>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><a href="#"><span class="white-text">%data%</span></a></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
