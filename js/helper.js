8c2285c3dcb878631bd58b28e7d487c3b62a9a6c
Eric0605 <tsang0605@gmail.com>
2018-01-14 21:29:18 -0800

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -16,12 +16,12 @@ var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span id="role">%data%</span>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><a href="#"><span class="white-text">%data%</span></a></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><a href="#"><span class="white-text">%data%</span></a></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-logo"><span class="orange-text">Twitter</span><a href="%link%"><img class="logo" src="https://designshack.net/wp-content/uploads/larrybird-2.jpg"></img></a></li>';
 var HTMLgithub = '<li class="flex-logo"><span class="orange-text">GitHub</span><a href="%link%"><img class="logo" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"></img></a></li>';
 var HTMLlinkin = '<li class="flex-logo"><span class="orange-text">LinkedIn</span><a href="%link%"><img class="logo" src="https://seeklogo.com/images/L/linkedin-icon-logo-FBADE03110-seeklogo.com.png"></img></a></li><hr>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><a href="#"><span class="white-text">%data%</span></a></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
