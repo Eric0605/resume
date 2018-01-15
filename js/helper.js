cfa66d603fa6604b052f820f3916519c9bf01da0
Eric0605 <tsang0605@gmail.com>
2018-01-14 21:02:57 -0800

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -31,7 +31,7 @@ var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<div>%data%</div>';
-var HTMLworkTitle = '<a href="%link%"><div>%data%</div></a>';
+var HTMLworkTitle = '<a href="%link%"><div id="blue-text">%data%</div></a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
