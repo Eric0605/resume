c6014decfc768c156da6c7b15e5cae5c1f6cfe17
Eric0605 <tsang0605@gmail.com>
2018-01-14 18:44:10 -0800

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -19,7 +19,7 @@ var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%conta
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><a href="#"><span class="white-text">%data%</span></a></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><a href="#"><span class="white-text">%data%</span></a></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">Twitter</span><a href="%link%"><span class="white-text">%data%</span></a></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">GitHub</span><a href="%link%"><img src = "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"></img></a></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">GitHub</span><a href="%link%"><img id="github" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"></img></a></li>';
 var HTMLlinkin = '<li class="flex-item"><span class="orange-text">LinkedIn</span><a href="%link%"><span class="white-text">%data%</span></a></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><a href="#"><span class="white-text">%data%</span></a></li>';
 
